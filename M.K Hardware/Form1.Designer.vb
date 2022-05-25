8f5123d1d60941680222adc07865873bba0370c8
Moobi Kabelo <giftmoobi@gmail.com>
2022-05-25 15:58:51 +0200

    intial update

---
+++ M.K Hardware/Form1.Designer.vb
@@ -0,0 +1,230 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Splitter1 = New System.Windows.Forms.Splitter()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
+        Me.LineShape1 = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtTankHeight = New System.Windows.Forms.TextBox()
+        Me.txtTankRadius = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.cmbTankShape = New System.Windows.Forms.ComboBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.rtbTankDetails = New System.Windows.Forms.RichTextBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Splitter1
+        '
+        Me.Splitter1.BackColor = System.Drawing.Color.CadetBlue
+        Me.Splitter1.Location = New System.Drawing.Point(0, 0)
+        Me.Splitter1.Name = "Splitter1"
+        Me.Splitter1.Size = New System.Drawing.Size(275, 427)
+        Me.Splitter1.TabIndex = 0
+        Me.Splitter1.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(250, 385)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'ShapeContainer1
+        '
+        Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
+        Me.ShapeContainer1.Name = "ShapeContainer1"
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.LineShape1})
+        Me.ShapeContainer1.Size = New System.Drawing.Size(895, 427)
+        Me.ShapeContainer1.TabIndex = 2
+        Me.ShapeContainer1.TabStop = False
+        '
+        'LineShape1
+        '
+        Me.LineShape1.Name = "LineShape1"
+        Me.LineShape1.X1 = 271
+        Me.LineShape1.X2 = 891
+        Me.LineShape1.Y1 = 30
+        Me.LineShape1.Y2 = 29
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.CadetBlue
+        Me.Label1.Font = New System.Drawing.Font("Mongolian Baiti", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(612, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(148, 23)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "M.K Hardware"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(527, 88)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(91, 16)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Tank Shape"
+        '
+        'txtTankHeight
+        '
+        Me.txtTankHeight.Location = New System.Drawing.Point(757, 124)
+        Me.txtTankHeight.Name = "txtTankHeight"
+        Me.txtTankHeight.Size = New System.Drawing.Size(121, 20)
+        Me.txtTankHeight.TabIndex = 10
+        '
+        'txtTankRadius
+        '
+        Me.txtTankRadius.Location = New System.Drawing.Point(530, 234)
+        Me.txtTankRadius.Name = "txtTankRadius"
+        Me.txtTankRadius.Size = New System.Drawing.Size(121, 20)
+        Me.txtTankRadius.TabIndex = 11
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.BackColor = System.Drawing.Color.Lime
+        Me.btnCalculate.Font = New System.Drawing.Font("Microsoft Tai Le", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(543, 316)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 13
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = False
+        '
+        'cmbTankShape
+        '
+        Me.cmbTankShape.FormattingEnabled = True
+        Me.cmbTankShape.Items.AddRange(New Object() {"Cone", "Cylinder"})
+        Me.cmbTankShape.Location = New System.Drawing.Point(530, 124)
+        Me.cmbTankShape.Name = "cmbTankShape"
+        Me.cmbTankShape.Size = New System.Drawing.Size(121, 21)
+        Me.cmbTankShape.TabIndex = 16
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(527, 203)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(94, 16)
+        Me.Label7.TabIndex = 18
+        Me.Label7.Text = "Tank Radius"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(754, 88)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(94, 16)
+        Me.Label8.TabIndex = 19
+        Me.Label8.Text = "Tank Height"
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.Red
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Tai Le", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(676, 374)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 20
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.Blue
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Tai Le", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(790, 316)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 21
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'rtbTankDetails
+        '
+        Me.rtbTankDetails.BackColor = System.Drawing.Color.CadetBlue
+        Me.rtbTankDetails.Location = New System.Drawing.Point(290, 49)
+        Me.rtbTankDetails.Name = "rtbTankDetails"
+        Me.rtbTankDetails.Size = New System.Drawing.Size(220, 348)
+        Me.rtbTankDetails.TabIndex = 22
+        Me.rtbTankDetails.Text = "Tank Shape    :" & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(10) & "Tank Height (mm) : " & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(10) & "Tank Radius (mm) :" & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(10) & "Tank Volume (Liter's) :" & _
+    " "
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(895, 427)
+        Me.Controls.Add(Me.rtbTankDetails)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.cmbTankShape)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtTankRadius)
+        Me.Controls.Add(Me.txtTankHeight)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Splitter1)
+        Me.Controls.Add(Me.ShapeContainer1)
+        Me.Name = "Form1"
+        Me.Text = "M.K Hardware"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Splitter1 As System.Windows.Forms.Splitter
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents ShapeContainer1 As Microsoft.VisualBasic.PowerPacks.ShapeContainer
+    Friend WithEvents LineShape1 As Microsoft.VisualBasic.PowerPacks.LineShape
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtTankHeight As System.Windows.Forms.TextBox
+    Friend WithEvents txtTankRadius As System.Windows.Forms.TextBox
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents cmbTankShape As System.Windows.Forms.ComboBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents rtbTankDetails As System.Windows.Forms.RichTextBox
+
+End Class
---
+++ M.K Hardware/Form1.vb
@@ -0,0 +1,53 @@
+﻿Public Class Form1
+    REM declare PI as global and intialize with pi
+    Const PI As Double = Math.PI
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Try
+            REM declare tank shape var and assign as string
+            Dim tankShape As String = cmbTankShape.Text
+            Dim tankHeight, tankRadius, tankVolume As Double
+
+            If tankShape.Length = 0 Then
+                MessageBox.Show("SHAPE ERROR: invalid data passed, please select tank shape", "VALUE ERROR!")
+            Else
+                If Double.TryParse(txtTankHeight.Text, tankHeight) Then
+                    If Double.TryParse(txtTankRadius.Text, tankRadius) Then
+                        REM check tank shape
+                        If tankShape = "Cone" Then
+                            REM calculate cylinder volume
+                            tankVolume = PI * (tankRadius * tankRadius) / tankHeight * 3
+                        Else
+                            REM calculate cylinder volume
+                            tankVolume = PI * (tankRadius * tankRadius) / tankHeight
+                        End If
+                        REM output tank volume
+                        rtbTankDetails.Text = String.Format("Tank Shape : {0}" & vbNewLine & vbNewLine & "Tank Height: {1} " & vbNewLine & vbNewLine & "Tank Radius: {2}" & vbNewLine & vbNewLine & "Tank Volume (Liter's) : {3}", tankShape, tankHeight, tankRadius, tankVolume.ToString("#.00"))
+                    Else
+                        MessageBox.Show("RADIUS ERROR: invalid data passed, please enter numeric value", "VALUE ERROR!")
+                    End If
+                Else
+                    MessageBox.Show("HEIGHT ERROR: invalid data passed, please enter numeric value", "VALUE ERROR!")
+                End If
+            End If
+        Catch ex As Exception
+            MessageBox.Show("CALCULATE ERROR, failed to calculate volume try again", "Error!")
+        End Try
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        cmbTankShape.Text = ""
+        txtTankHeight.Text = ""
+        txtTankRadius.Text = ""
+        rtbTankDetails.Text = "Tank Shape : " & vbNewLine & vbNewLine & "Tank Height : " & vbNewLine & vbNewLine & "Tank Radius : "
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Dim existStatus As DialogResult = MessageBox.Show("Are you sure you want to exit ?", "Warning", MessageBoxButtons.YesNo)
+
+        If existStatus = DialogResult.Yes Then
+            Me.Close()
+        End If
+    End Sub
+
+End Class
